dfb3da28ababd80f049a8def6e9d63170062bc25
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-07-14 20:05:26 -0500

    added files

--- index.js
+++ index.js
@@ -8,61 +8,63 @@ const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [{
         type: "input",
-        message: "What is your GiyHub username?",
+        message: "What is your GitHub username?",
         name: "username",
-    }, {
-        type: 'input',
-        message: 'What is the title of this project?',
-        name: 'title',
     },
     {
-        type: 'input',
-        message: 'Please enter a project description:',
-        name: 'description',
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
     },
     {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'install',
+        type: "input",
+        message: "What is the title of this project?",
+        name: "title",
     },
     {
-        type: 'input',
-        message: 'Please provide usage information:',
-        name: 'usage',
+        type: "input",
+        message: "Please enter a project description:",
+        name: "description",
     },
     {
-        type: 'input',
-        message: 'Please enter any contribution guidelines:',
-        name: 'contribution',
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
     },
     {
-        type: 'input',
-        message: 'Please provide test instructions:',
-        name: 'usage',
+        type: "input",
+        message: "Please provide usage information:",
+        name: "usage",
     },
     {
-        type: 'input',
-        message: 'What type of license does this project use?',
-        name: 'license',
+        type: "input",
+        message: "Please enter any contribution guidelines:",
+        name: "contribution",
     },
-
+    {
+        type: "input",
+        message: "Please provide test instructions:",
+        name: "test",
+    },
+    {
+        type: "checkbox",
+        message: "What type of license does this project use?",
+        name: "license",
+        choices: [" MIT License", "Apache 2.0 License", "Boost 1.0 License"],
+    },
 ];
 
 // TODO: Create a function to write README file
 inquirer.prompt(questions).then((responses) => {
-    fs.writeFile("README.md", generateMarkdown({...responses }), (err) =>
+    fs.writeFile("README1.md", generateMarkdown({...responses }), (err) =>
         err ? console.log(err) : console.log("Success!")
     );
 
-    console.log(data.firstName);
-    console.log(data.password);
-    console.log(data.city);
 });
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// If there is no license, return  "unlock"
+function renderLicenseBadge(license) {
+    if (`${license}` == "MIT") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (`${license}` == " Apache 2.0 License") {
+        return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (`${license}` == "Boost 1.0") {
+        return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// If there is no license, return "unlock"
+function renderLicenseSection(license) {
+    if (`${license}` == "unlock") {
+        return `it is not covered by a license`
+    } else {
+        return `it is covered with ${license}license`
+    }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
+function generateMarkdown(responses) {
+    return `# ${responses.title}
+    #License
+    ${renderLicenseBadge(responses.license)}
+    ${renderLicenseSection(responses.license)}
+     
+    #Description
+    ${responses.description}
 
-`;
+
+    # Table of Contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[Contributors](#contributors)
+    *[Test](#test)
+    *[Questions](#questions)
+
+    #Installation
+    ${responses.installation}
+
+    #Usage
+    ${responses.usage}
+
+    #Contributing
+    ${responses.contribution}
+
+    #Test
+    ${responses.test}
+
+    #Questions
+    Created by ${responses.username}
+    <br>
+    Email:${responses.email}
+    `;
 }
 
 module.exports = generateMarkdown;
